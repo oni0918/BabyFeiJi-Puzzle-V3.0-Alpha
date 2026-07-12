cad10992bf774c56d782e6ad2ab12bdd5aada49a
oni0918 <oni0918@gmail.com>
2026-07-13 03:39:09 +0800

    Add Android app build config

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.babyfeiji.puzzlev3"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.babyfeiji.puzzlev3"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 30
+        versionName = "3.0-alpha"
+    }
+}
